22d5659d965e688329a81d8bca9d001cb6e5cb41
DESKTOP-RN2K5O7\Yashwanth <gupta.yashwanth@gmail.com>
2018-03-13 01:44:26 +0100

    initial commit

---
+++ Priodic/DataModel/SampleDataSource.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Storage;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The data model defined by this file serves as a representative example of a strongly-typed
+// model.  The property names chosen coincide with data bindings in the standard item templates.
+//
+// Applications may use this model as a starting point and build on it, or discard it entirely and
+// replace it with something appropriate to their needs. If using this model, you might improve app 
+// responsiveness by initiating the data loading task in the code behind for App.xaml when the app 
+// is first launched.
+
+namespace Priodic.Data
+{
+    /// <summary>
+    /// Generic item data model.
+    /// </summary>
+    public class SampleDataItem
+    {
+        public SampleDataItem(String uniqueId, String title,String name,String number,String weight,String density,String radius,String electron,String ion,String oxy, String inf,String imagePath)
+        {
+            this.UniqueId = uniqueId;
+            this.Title = title;
+            this.Name = name;
+            this.Number = number;
+            this.Weight = weight;
+            this.Density = density;
+            this.Radius = radius;
+            this.Electron = electron;
+            this.Ion = ion;
+            this.Oxy = oxy;
+            this.Inf = inf;
+            this.ImagePath = imagePath;
+           
+        }
+
+        public string UniqueId { get; private set; }
+        public string Title { get; private set; }
+        public string Name { get; private set; }
+        public string Number { get; private set; }
+        public string Weight { get; private set; }
+        public string Density { get; private set; }
+        public string Radius { get; private set; }
+        public string Electron { get; private set; }
+        public string Ion { get; private set; }
+        public string Oxy { get; private set; }
+        public string Inf { get; private set; }
+        public string ImagePath { get; private set; }
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }
+
+    /// <summary>
+    /// Generic group data model.
+    /// </summary>
+    public class SampleDataGroup
+    {
+        public SampleDataGroup(String uniqueId, String title, String subtitle, String imagePath, String description)
+        {
+            this.UniqueId = uniqueId;
+            this.Title = title;
+            this.Subtitle = subtitle;
+            this.Description = description;
+            this.ImagePath = imagePath;
+            this.Items = new ObservableCollection<SampleDataItem>();
+        }
+       
+
+        
+        public string UniqueId { get; private set; }
+        public string Title { get; private set; }
+        public string Subtitle { get; private set; }
+        public string Description { get; private set; }
+        public string ImagePath { get; private set; }
+        public ObservableCollection<SampleDataItem> Items { get; private set; }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+    }
+
+    /// <summary>
+    /// Creates a collection of groups and items with content read from a static json file.
+    /// 
+    /// SampleDataSource initializes with data read from a static json file included in the 
+    /// project.  This provides sample data at both design-time and run-time.
+    /// </summary>
+    public sealed class SampleDataSource
+    {
+        private static SampleDataSource _sampleDataSource = new SampleDataSource();
+
+        private ObservableCollection<SampleDataGroup> _groups = new ObservableCollection<SampleDataGroup>();
+        public ObservableCollection<SampleDataGroup> Groups
+        {
+            get { return this._groups; }
+        }
+
+        public static async Task<IEnumerable<SampleDataGroup>> GetGroupsAsync()
+        {
+            await _sampleDataSource.GetSampleDataAsync();
+
+            return _sampleDataSource.Groups;
+        }
+
+        public static async Task<SampleDataGroup> GetGroupAsync(string uniqueId)
+        {
+            await _sampleDataSource.GetSampleDataAsync();
+            // Simple linear search is acceptable for small data sets
+            var matches = _sampleDataSource.Groups.Where((group) => group.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        public static async Task<SampleDataItem> GetItemAsync(string uniqueId)
+        {
+            await _sampleDataSource.GetSampleDataAsync();
+            // Simple linear search is acceptable for small data sets
+            var matches = _sampleDataSource.Groups.SelectMany(group => group.Items).Where((item) => item.UniqueId.Equals(uniqueId));
+            if (matches.Count() == 1) return matches.First();
+            return null;
+        }
+
+        private async Task GetSampleDataAsync()
+        {
+            if (this._groups.Count != 0)
+                return;
+
+            Uri dataUri = new Uri("ms-appx:///DataModel/SampleData.json");
+
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(dataUri);
+            string jsonText = await FileIO.ReadTextAsync(file);
+            JsonObject jsonObject = JsonObject.Parse(jsonText);
+            JsonArray jsonArray = jsonObject["Groups"].GetArray();
+           
+
+
+            foreach (JsonValue groupValue in jsonArray)
+            {
+                JsonObject groupObject = groupValue.GetObject();
+                SampleDataGroup group = new SampleDataGroup(groupObject["UniqueId"].GetString(),
+                                                            groupObject["Title"].GetString(),
+                                                            groupObject["Subtitle"].GetString(),
+                                                            groupObject["ImagePath"].GetString(),
+                                                            groupObject["Description"].GetString());
+
+                foreach (JsonValue itemValue in groupObject["Items"].GetArray())
+                {
+                    JsonObject itemObject = itemValue.GetObject();
+                    group.Items.Add(new SampleDataItem(itemObject["UniqueId"].GetString(),
+                                                       itemObject["Title"].GetString(),
+                                                       itemObject["Name"].GetString(),
+                                                       itemObject["Number"].GetString(),
+                                                       itemObject["Weight"].GetString(),
+                                                       itemObject["Density"].GetString(),
+                                                       itemObject["Radius"].GetString(),
+                                                       itemObject["Electron"].GetString(),
+                                                       itemObject["Ion"].GetString(),
+                                                       itemObject["Oxy"].GetString(),
+                                                       itemObject["Inf"].GetString(),
+                                                       itemObject["ImagePath"].GetString()));
+                }
+                this.Groups.Add(group);
+            }
+        }
+    }
+}
